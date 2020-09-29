acc4694f2f8e18637f7f09d08ea4b48070f057aa
Ray Bray <rbray.bray2@gmail.com>
2020-09-29 17:41:30 -0400

    ran app.js file with node and it works.

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const Employee_Ids = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+console.log("Please build your team")
+
 buildManager();
 
 function buildManager() {
